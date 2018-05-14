9187e9a1fff8b113f69ea600664508249145cfb6
Jeremy Rasor <jeremyrasor97@gmail.com>
2018-05-14 17:36:54 -0400

    Change heading 2 to the text entered in the form when submit is clicked

--- index.js
+++ index.js
@@ -1,9 +1,26 @@
 const button = document.querySelector('button.greeting')
 
+const form = document.querySelector('form')
+
 function changeText(ev) {
     
     document.querySelector('#secondHeading').textContent = 'You changed the second heading text'
 
 }
 
+function changeTextCustom(ev) {
+
+    const secondHeading = document.querySelector('#secondHeading')
+
+    const formInputText = ev.target.querySelector('input').value
+
+    secondHeading.textContent = formInputText
+
+    //this will prevent the submit button from 'refreshing' the page
+    ev.preventDefault()
+
+}
+
 button.addEventListener('click', changeText)
+
+form.addEventListener('submit', changeTextCustom)
