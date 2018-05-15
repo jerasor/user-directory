8406ea3e713964283ae07745365b582fd6678e17
Jeremy Rasor <jeremyrasor97@gmail.com>
2018-05-15 16:55:31 -0400

    Create list through renderList, and allow renderListItem to take in an array of children to add

--- index.js
+++ index.js
@@ -18,7 +18,12 @@ const renderListItem = function(text, childToAppend) {
 
     if (childToAppend !== undefined) {
         
-        listItem.appendChild(childToAppend)
+        for (let index = 0; index < childToAppend.length; index++) {
+            
+            const child = childToAppend[index];
+            listItem.appendChild(child)
+            
+        }
 
     }
 
@@ -26,29 +31,38 @@ const renderListItem = function(text, childToAppend) {
 
 }
 
+const renderList = function (name, age, color) {
+
+    const nameItem = renderListItem(`Name: ${name}`)
+
+    const ageItem = renderListItem(`Age: ${age}`)
+
+    const colorItem = renderListItem('Favorite Color: ', [renderColor(color)])
+
+    const list = document.createElement('ul')
+    list.appendChild(nameItem)
+    list.appendChild(ageItem)
+    list.appendChild(colorItem)
+
+    return list
+
+}
+
 const handleSubmit = function(ev) {
-  ev.preventDefault()
-  const f = ev.target
-  const userName = f.userName.value
-  const age = f.age.value
-  const favoriteColor = f.favoriteColor.value
+    
+    ev.preventDefault()
+    const f = ev.target
+    const userName = f.userName.value
+    const age = f.age.value
+    const favoriteColor = f.favoriteColor.value
 
-  const nameItem = renderListItem(`Name: ${userName}`)
+    const list = renderList(userName, age, favoriteColor)
 
-  const ageItem = renderListItem(`Age: ${age}`)
+    const users = document.querySelector('#users')
+    users.appendChild(list)
 
-  const colorItem = renderListItem('Favorite Color: ', renderColor(favoriteColor))
-
-  const list = document.createElement('ul')
-  list.appendChild(nameItem)
-  list.appendChild(ageItem)
-  list.appendChild(colorItem)
-
-  const users = document.querySelector('#users')
-  users.appendChild(list)
-
-  f.reset()
-  f.userName.focus()
+    f.reset()
+    f.userName.focus()
 }
 
 form.addEventListener('submit', handleSubmit)
