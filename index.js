546300d7bc9e77f18fa0aa17bf249456b95f5a9c
Jeremy Rasor <jeremyrasor97@gmail.com>
2018-05-15 15:50:53 -0400

    Get caught up with class

--- index.js
+++ index.js
@@ -1,42 +1,21 @@
-//getting the greeting button
-const button = document.querySelector('button.greeting')
+const form = document.querySelector('#userForm')
 
-//getting the form
-const form = document.querySelector('form')
+const handleSubmit = function(ev) {
+  ev.preventDefault()
+  const users = document.querySelector('#users')
+  const f = ev.target
+  const userName = f.userName.value
+  const age = f.age.value
+  const favoriteColor = f.favoriteColor.value
 
-//change the text of the second heading element to a 
-//default response
-function changeText(ev) {
-    
-    document.querySelector('#secondHeading').textContent = 'You changed the second heading text'
+  const p = document.createElement('p')
+  p.textContent = `${userName}, ${age}`
+  p.style.backgroundColor = favoriteColor
 
+  users.appendChild(p)
+
+  f.reset()
+  f.userName.focus()
 }
 
-//change the text of the second heading element 
-//to a custon response entered in the form
-function changeTextCustom(ev) {
-
-    //the second heading
-    const secondHeading = document.querySelector('#secondHeading')
-
-    //the text from the input textbox from the form
-    const formInputText = document.querySelector('#textToChange').value
-
-    //setting the second heading text to the form's input text
-    secondHeading.textContent = formInputText
-
-    //prevents the form from being submitted
-    ev.preventDefault()
-
-    //resetting the form after submission
-    ev.target.reset()
-
-}
-
-//adding click event listener to the default 
-//greetting button
-button.addEventListener('click', changeText)
-
-//adding a submit listener to the form (the submit process
-//will be blocked in changeTextCustom)
-form.addEventListener('submit', changeTextCustom)
+form.addEventListener('submit', handleSubmit)
