24b50efe3b6b7e3203ed9929d95c387c0547259d
Jeremy Rasor <jeremyrasor97@gmail.com>
2018-05-15 09:37:31 -0400

    Update files with explanatory comments and change the form back to a submit button because if enter was hit (instead of clicking submit button) the form would submit anyways so this would produce undesired behavior of a 'page refresh'

--- index.js
+++ index.js
@@ -1,23 +1,39 @@
+//getting the greeting button
 const button = document.querySelector('button.greeting')
 
-const formButton = document.querySelector('#formSubmitButton')
+//getting the form
+const form = document.querySelector('form')
 
+//change the text of the second heading element to a 
+//default response
 function changeText(ev) {
     
     document.querySelector('#secondHeading').textContent = 'You changed the second heading text'
 
 }
 
+//change the text of the second heading element 
+//to a custon response entered in the form
 function changeTextCustom(ev) {
 
+    //the second heading
     const secondHeading = document.querySelector('#secondHeading')
 
+    //the text from the input textbox from the form
     const formInputText = document.querySelector('#textToChange').value
 
+    //setting the second heading text to the form's input text
     secondHeading.textContent = formInputText
+
+    //prevents the form from being submitted
+    ev.preventDefault()
 
 }
 
+//adding click event listener to the default 
+//greetting button
 button.addEventListener('click', changeText)
 
-formButton.addEventListener('click', changeTextCustom)
+//adding a submit listener to the form (the submit process
+//will be blocked in changeTextCustom)
+form.addEventListener('submit', changeTextCustom)
